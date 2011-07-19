0d7bd7fa242de424b0949f09739133d599ab9231
FastLabs <bulavitchi@gmail.com>
2011-07-19 23:54:40 +0100

    implemented setter method

--- src/org/flabs/bucket/Bucket.groovy
+++ src/org/flabs/bucket/Bucket.groovy
@@ -3,4 +3,9 @@ package org.flabs.bucket
 class Bucket extends Expando {
     String identifier
 
+    @Override
+    def void setProperty(String property, Object newValue) {
+         super.setProperty(new Petal(payload: newValue))
+    }
+
 }
